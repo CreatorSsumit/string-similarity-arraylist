d89ac30f480fb4071d1e61eb64b7fa3b587fe882
Sumit Kumar Kushwaha <pksumitkumar98@gmail.com>
2023-10-01 16:03:00 +0530

    add spacy

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
+const { load } = require('spacy');
 
-function jaccardSimilarity(set1, set2) {
+
+async function jaccardSimilarity(set1, set2) {
+
+
+  const spacy = await load('en_core_web_sm'); // Load the English language model
+
+  const doc1 = await spacy(set1);
+  const doc2 = await spacy(set2);
+
+  const similarity = doc1.similarity(doc2);
+  console.log(similarity)
     
-    first = set1.replace(/\s+/g, '')
-	second = set2.replace(/\s+/g, '')
+  //   first = set1.replace(/\s+/g, '')
+	// second = set2.replace(/\s+/g, '')
 
-	if (first === second) return 1; // identical or empty
-	if (first.length < 2 || second.length < 2) return 0; // if either is a 0-letter or 1-letter string
+	// if (first === second) return 1; // identical or empty
+	// if (first.length < 2 || second.length < 2) return 0; // if either is a 0-letter or 1-letter string
 
-	let firstBigrams = new Map();
-	for (let i = 0; i < first.length - 1; i++) {
-		const bigram = first.substring(i, i + 2);
-		const count = firstBigrams.has(bigram)
-			? firstBigrams.get(bigram) + 1
-			: 1;
+	// let firstBigrams = new Map();
+	// for (let i = 0; i < first.length - 1; i++) {
+	// 	const bigram = first.substring(i, i + 2);
+	// 	const count = firstBigrams.has(bigram)
+	// 		? firstBigrams.get(bigram) + 1
+	// 		: 1;
 
-		firstBigrams.set(bigram, count);
-	};
+	// 	firstBigrams.set(bigram, count);
+	// };
 
-	let intersectionSize = 0;
-	for (let i = 0; i < second.length - 1; i++) {
-		const bigram = second.substring(i, i + 2);
-		const count = firstBigrams.has(bigram)
-			? firstBigrams.get(bigram)
-			: 0;
+	// let intersectionSize = 0;
+	// for (let i = 0; i < second.length - 1; i++) {
+	// 	const bigram = second.substring(i, i + 2);
+	// 	const count = firstBigrams.has(bigram)
+	// 		? firstBigrams.get(bigram)
+	// 		: 0;
 
-		if (count > 0) {
-			firstBigrams.set(bigram, count - 1);
-			intersectionSize++;
-		}
-	}
+	// 	if (count > 0) {
+	// 		firstBigrams.set(bigram, count - 1);
+	// 		intersectionSize++;
+	// 	}
+	// }
 
-	return (2.0 * intersectionSize) / (first.length + second.length - 2);
+	// return (2.0 * intersectionSize) / (first.length + second.length - 2);
 }
 
 
